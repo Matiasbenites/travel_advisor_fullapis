050ab2794cafa574a4a3e767cbc7c285e28edec1
Matute <79936247+Matiasbenites@users.noreply.github.com>
2023-01-07 20:19:54 -0300

    Map's component added

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuerry } from "@mui/material";
+import LocactionOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import Rating from "@mui/lab";
+
+import useStyles from "./styles.js";
 
 const Map = () => {
-  return <h1>Map</h1>;
+  const classes = useStyles();
+  const isMobile = useMediaQuerry("(main-width:600px)");
+
+  const coordinates = { lat: 0, lng: 0 };
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
